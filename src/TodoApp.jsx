998503730ede5210dd8f56c4ff76ccd5beb11b77
purplevarun <purplevarun@gmail.com>
2022-02-02 19:08:34 +0530

    two columns for main note area and saved notes

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
-
+import React from "react";
+import "./TodoApp.css";
 const TodoApp = () => {
-  return <div>
-    hey baby
-  </div>;
+	return (
+		<div className="Main">
+			<div className="todoapp">
+				<div className="new-note-area">
+					<textarea
+						name="newnite"
+						id=""
+						cols="40"
+						rows="10"
+						placeholder="Write new note here.."
+					></textarea>
+				</div>
+				<div className="submit-area">
+					<button className="submitbtn">Save this note</button>
+				</div>
+			</div>
+			<div className="saved-notes">hey</div>
+		</div>
+	);
 };
 
 export default TodoApp;
